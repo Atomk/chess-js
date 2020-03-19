b84155c27e85ed7d7de807bb21519aaa68fbd423
Atomk <marco.fras@hotmail.it>
2020-03-19 14:55:07 +0100

    refactor: clarify X and Y meaning, global grid size

--- chess.js
+++ chess.js
@@ -1,27 +1,28 @@
+const GRID_SIZE = 8;
+
 document.body.onload = function() {
-    let grid = createGridElement();
+    let grid = createGridElement(GRID_SIZE);
     document.getElementById("grid-container").appendChild(grid);
 }
 
-function createGridElement() {
-    let gridSize = 8;
+function createGridElement(gridSize) {
     let table = document.createElement("table");
 
     // Determines color of the first cell in the row
     // The cell up left should be white
     let firstTRCellWhite = true;
 
-    for(let y = 0; y < gridSize; y++) {
+    for(let row = 0; row < gridSize; row++) {
         let tr = document.createElement("tr");
         
-        for(let x = 0; x < gridSize; x++) {
+        for(let col = 0; col < gridSize; col++) {
             let td = document.createElement("td");
 
             // Give the right color to the cell
             if(firstTRCellWhite)
-                td.className = x % 2 === 0 ? "cell-white" : "cell-black";
+                td.className = col % 2 === 0 ? "cell-white" : "cell-black";
             else
-                td.className = x % 2 === 0 ? "cell-black" : "cell-white";
+                td.className = col % 2 === 0 ? "cell-black" : "cell-white";
 
             tr.appendChild(td);
         }
